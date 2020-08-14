035a8da6067ac2d195f6cd7d47d102c0d61e9b44
Joseph Wilk <joe@josephwilk.net>
2020-08-14 22:19:11 +0100

    Test Demos

--- README.md
+++ README.md
@@ -12,3 +12,22 @@ Output: 20 GPIO pins
 ```
 pip3 install -r requirements.txt
 ```
+
+
+## Setup
+
+Solenoid 1: Air in  - Right Neck - UP
+Solenoid 2: Air out - Right Neck - DOWN
+Solenoid 3: Air in  - Canopy Fingers right
+Solenoid 4: Air out - Canopy Fingers right
+Solenoid 5: Air in  - Center canopy - (engages both fingers and waist)
+Solenoid 6: Air out - Center canopy
+Solenoid 7: Air in  - Flower petals - Shut
+Solenoid 8: Air out - Flower petals - Open
+
+Bench (Solenoid per bench) TODO
+
+Solenoid 9  - Air in  - Right side
+Solenoid 10 - Air out - Right side
+Solenoid 11 - Air in  - Left side
+Solenoid 12 - Air out - Left side.
--- playable_space.py
+++ playable_space.py
@@ -1,6 +1,14 @@
 from time import sleep
 from gpiozero import LED
 
+SOLENOID = {
+    "right_neck":     {"up":   1, "down": 2}
+    "canopy_fingers": {"open": 3, "close": 4}
+    "canopy_center":  {"open": 5, "close": 6}
+    "flowers":        {"open": 8, "close": 7}
+    "bench":          {}
+}
+
 class PlayableSpace:
     def __init__(self):
         self.pings = []
@@ -17,3 +25,38 @@ class PlayableSpace:
             sleep(1000)
             pin.off()
             sleep(1000)
+
+    def up(self, area):
+        pins[SOLENOID[area]["up"]].on()
+
+    def down(self, area):
+        pins[SOLENOID[area]["down"]].off()
+
+    def open(self, area):
+        pins[SOLENOID[area]["open"]].on()
+
+    def close(self, area):
+        pins[SOLENOID[area]["close"]].off()
+
+    def demo(self):
+        #UP
+        up("right_neck")
+        sleep(1000 * 12)
+        open("canopy_fingers")
+        sleep(1000 * 8)
+
+        open("canopy_center")
+        sleep(1000 * 12)
+
+        open("flowers")
+        sleep(1000 * 10)
+
+        #DOWN
+        close("flowers")
+        sleep(1000 * 10)
+        close("canopy_center")
+        sleep(1000 * 18)
+        close("canopy_fingers")
+        sleep(1000 * 18)
+        down("right_neck")
+        sleep(1000 * 18)
